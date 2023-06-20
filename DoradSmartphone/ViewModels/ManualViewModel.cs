d8f72841a0d9230470e6bf221456d3d6fc571936
Luiz Costa <lcosta@lusospace.com>
2023-06-20 18:09:07 +0100

    Drag and drop feature working for manual selection

--- DoradSmartphone/ViewModels/ManualViewModel.cs
+++ DoradSmartphone/ViewModels/ManualViewModel.cs
@@ -1,20 +1,17 @@
-﻿using DoradSmartphone.Models;
+﻿using CommunityToolkit.Mvvm.Input;
+using DoradSmartphone.Models;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Windows.Input;
-using static Android.Icu.Text.CaseMap;
-using static Microsoft.Maui.LifecycleEvents.AndroidLifecycle;
 
 namespace DoradSmartphone.ViewModels
 {
     public partial class ManualViewModel : BaseViewModel, INotifyPropertyChanged
     {
-        public ObservableCollection<Widget> WidgetsToOrganize { get; set; }
-
-        public Command<Widget> WidgetDraggedCommand { get; }
-        public Command<Widget> WidgetDroppedCommand { get; }
+        //public ICommand LoadAutomaticPageCommand => new Command(LoadAutomaticPage);
 
         private Widget draggedWidget;
+
+        private int selectedWidget;
 
         private ObservableCollection<Widget> widgets;
         public ObservableCollection<Widget> Widgets
@@ -34,26 +31,77 @@ namespace DoradSmartphone.ViewModels
         {
             Title = "Manual Configuration";
             Widgets = new ObservableCollection<Widget>(selectedItems);
-
-            WidgetsToOrganize = new ObservableCollection<Widget>(widgets);
-            WidgetDraggedCommand = new Command<Widget>(OnWidgetDragged);
-            WidgetDroppedCommand = new Command<Widget>(OnWidgetDropped);
         }
 
-        private void OnWidgetDragged(Widget widget)
+        [RelayCommand]
+        public void Drag(Widget widget)
         {
             draggedWidget = widget;
         }
 
-        private void OnWidgetDropped(Widget widget)
+        [RelayCommand]
+        public void Drop(string option)
         {
-            if (draggedWidget != null)
-            {
-                // Perform any necessary actions when the drop occurs
-                // Update the position properties of the draggedWidget (PosX, PosY)
+            int op = Convert.ToInt32(option);
+            if (selectedWidget == op) return;
 
-                draggedWidget = null;
+            if(draggedWidget != null)
+            {
+                var currentWidget = widgets[selectedWidget];               
             }
         }
+
+        [RelayCommand]
+        private void LoadAutomaticPage()
+        {
+            var layout = new Grid();
+
+            int numColumns = (int)Math.Ceiling(Math.Sqrt(Widgets.Count));
+            int numRows = (int)Math.Ceiling((double)Widgets.Count / numColumns);
+
+            for (int row = 0; row < numRows; row++)
+            {
+                layout.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            }
+
+            for (int col = 0; col < numColumns; col++)
+            {
+                layout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            }
+
+            int widgetIndex = 0;
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int col = 0; col < numColumns; col++)
+                {
+                    if (widgetIndex < Widgets.Count)
+                    {
+                        var widget = Widgets[widgetIndex];
+                        var image = new Image
+                        {
+                            Source = widget.FileName,
+                            Aspect = Aspect.AspectFit
+                        };
+
+                        Grid.SetRow(image, row);
+                        Grid.SetColumn(image, col);
+                        layout.Children.Add(image);
+
+                        widgetIndex++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            AutomaticPage = new ContentPage
+            {
+                Title = "Automatic",
+                Content = layout
+            };
+        }
+
     }
 }
--- DoradSmartphone/Views/ManualPage.xaml.cs
+++ DoradSmartphone/Views/ManualPage.xaml.cs
@@ -1,13 +1,43 @@
 using DoradSmartphone.Models;
 using DoradSmartphone.ViewModels;
+using ToastProject;
 
 namespace DoradSmartphone.Views;
 
 public partial class ManualPage : ContentPage
 {
-	public ManualPage(List<Widget> selectedItems)
-	{
-        InitializeComponent();
+    private Point initialPosition;
+    public ManualPage(List<Widget> selectedItems)
+    {
+        InitializeComponent();        
         BindingContext = new ManualViewModel(selectedItems);
+    }
+
+    private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+        var image = (Image)sender;
+
+        switch (e.StatusType)
+        {
+            case GestureStatus.Started:
+                // Store the initial position of the image
+                initialPosition = new Point(image.TranslationX, image.TranslationY);
+                break;
+
+            case GestureStatus.Running:
+                // Move the image based on the pan offset
+                image.TranslationX = initialPosition.X + e.TotalX;
+                image.TranslationY = initialPosition.Y + e.TotalY;
+                break;
+
+            case GestureStatus.Completed:
+                // Get the final position of the image when the dragging is completed
+                var finalPosition = new Point(image.TranslationX, image.TranslationY);
+
+                Shell.Current.DisplayAlert("Warning", "The position is: " + finalPosition, "Ok");
+                // Perform any desired actions with the final position
+                // ...
+                break;
+        }
     }
 }
